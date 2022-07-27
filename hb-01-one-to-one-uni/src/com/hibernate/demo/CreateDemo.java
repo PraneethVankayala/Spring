b239d4557bbcb7dfc8013545134782ad86ce5984
PraneethVankayala <bspraneethvankayala@gmail.com>
2022-07-27 23:13:42 +0530

    One-To-One-Unidirectional

---
+++ hb-01-one-to-one-uni/src/com/hibernate/demo/CreateDemo.java
@@ -0,0 +1,47 @@
+package com.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.demo.entity.Instructor;
+import com.hibernate.demo.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+		//create Session Factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//create the objects
+			Instructor tmpInstructor = new Instructor("Chad","Darby","darby@luv2code.com");
+			InstructorDetail tmpInstructorDetail = 
+						new InstructorDetail("http://www.luv2code.com/youtube","Coding");
+			
+			// associate the objects
+			tmpInstructor.setInstructorDetail(tmpInstructorDetail);
+			
+			//start Transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			// This will also save the details object because of CascadeType.ALL
+			System.out.println("Saving instructor: "+tmpInstructor.toString());
+			session.save(tmpInstructor);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!");
+		} finally {
+			factory.close();
+		}
+	}
+
+}
